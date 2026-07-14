c9c6b8bb2f7d84b95e6b1b48d4cb96dbdec820d4
maklevia <maria.klevchyk@gmail.com>
2026-07-14 16:44:05 +0300

    Added axios interceptor to validate refresh tokens

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,10 +1,30 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 
- export const api: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: { 'Content-Type': 'application/json' },
-    timeout: 10000,
-    withCredentials: true
- }) 
+export const api: AxiosInstance = axios.create({
+  baseURL: "http://localhost:3001",
+  headers: { "Content-Type": "application/json" },
+  timeout: 10000,
+  withCredentials: true,
+});
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      error.config.url !== "/auth/refresh"
+    ) {
+      try {
+        await api.post("/auth/refresh");
+        return api(error.config);
+      } catch {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
