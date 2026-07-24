c6b43619007a6f2793bd9acab5d0c53e1daf9787
maklevia <maria.klevchyk@gmail.com>
2026-07-24 20:49:48 +0300

    Some fixes

--- telegram-bot/src/handlers/onStartCommand.ts
+++ telegram-bot/src/handlers/onStartCommand.ts
@@ -1,5 +1,5 @@
-import { Context } from "telegraf";
 import axios from "axios";
+import { Context } from "telegraf";
 import { getEnvOrThrow } from "@bot/utils/getEnvOrThrow.js";
 
 const API_URL = getEnvOrThrow("API_ORIGIN");
